79844e9891658a60f6022d527142b54e7fceceb3
wenjin1997 <71911495+wenjin1997@users.noreply.github.com>
2021-04-22 23:52:04 +0800

    Add files via upload

---
+++ MaximumSubarray2.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class MaximumSubarray2 {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] nums = new int[n];
+		for(int l = 0; l < n; l++)
+		{
+			nums[l] = in.nextInt();
+		}
+		
+		int thisSum = 0, maxSum = nums[0];
+		for(int i = 0; i < n; i++) // 子列的左端
+		{
+			for(int j = i; j < n; j++) // 子列的右端
+			{
+				thisSum = 0;
+				thisSum += nums[j];
+				if(thisSum >= maxSum)
+				{
+					maxSum = thisSum;
+				}
+			}
+		}
+		System.out.println(maxSum);
+	}
+}
